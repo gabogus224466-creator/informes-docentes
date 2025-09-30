d14aa900e8a88cb9aad6de2807901ce1c009b838
gabogus224466-creator <gabogus224466@gmail.com>
2025-09-29 20:37:27 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,7 +1,8 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+// lib/supabase-browser.ts
+import { createClient } from '@supabase/supabase-js';
 
 export const supaBrowser = () =>
-  createBrowserClient(
+  createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
